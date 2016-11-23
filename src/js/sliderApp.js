d0eedc2e3c3f935ae700dc02c2790d8bbdfc6927
sgattani <saurabh.gattani@gmail.com>
2016-11-23 06:30:33 +0530

    modified files

--- src/js/sliderApp.js
+++ src/js/sliderApp.js
@@ -10,12 +10,13 @@ sliderComponent.controller('sliderCtrl', ['$scope', '$sce', 'carouselService',
     function($scope, $sce, carouselService) {
 		$scope.items = [];
 		var init = function() {
-			// fetch carousel data
+			// init all params
 			var param = {};
 			$scope.showContentPane = true;
 			$scope.currentIndex = 0;
 			$scope.heading = '';
 			
+			// web service call to fetch data from json
 			carouselService.getData(param)
                     .success(function(response){
 						console.log(response);
@@ -31,10 +32,12 @@ sliderComponent.controller('sliderCtrl', ['$scope', '$sce', 'carouselService',
 		init();
 		
 		$scope.getHtml = function(html){
+			// strict contextual escaping for marking the json data HTML as safe
 			return $sce.trustAsHtml(html);
 		};
 		
 		$scope.previous = function() {
+			// decrement index on previous arrow click and wrap around on boundary condition
 			if ($scope.currentIndex > 0) {
 				$scope.currentIndex--;
 			}
@@ -44,11 +47,13 @@ sliderComponent.controller('sliderCtrl', ['$scope', '$sce', 'carouselService',
 		};
 		
 		$scope.next = function() {
-			var numItems = $scope.items.length - 1;
+			var numItems = $scope.items.length;
+			// increment index on next arrow click and wrap around on boundary condition
 			$scope.currentIndex = ($scope.currentIndex + 1) % numItems;
 		};
 		
 		$scope.toggleView = function() {
+			// hide and show widget for handling expand/collapse logic
 			$scope.showContentPane = !$scope.showContentPane;
 		}
 		
@@ -58,12 +63,14 @@ sliderComponent.controller('sliderCtrl', ['$scope', '$sce', 'carouselService',
 
 sliderComponent.filter('html', function($sce) {
     return function(val) {
+		// return safe HTML by strict contextual escaping
         return $sce.trustAsHtml(val);
     };
 });
 
 // web service
 sliderComponent.service('carouselService', ['$http', function($http) {
+	// angularjs $http service call to fetch carousel data via json
     this.getData = function (param) {
         return $http.get('/angularjs-carousel/content.json', param);
     }
